3996822a170a688fee0dc4d9a98c727d062cf6df
Haku <johnmigzreyes08@example.com>
2025-02-23 21:35:47 +0800

    MIGZ REYES
    CHANGES
    
    res/layout added
    - activity_account.xml
    - activity_contact_us.xml
    - activity_order_history.xml
    - activity_terms_and_condition.xml
    - side_nav.xml
    - toolbar.xml
    
    java folder (backend) added
    - account.java
    - orderHistory.java
    - contactUs.java
    - termsAndCondition.java

--- app/src/main/java/com/example/buildyourownmeal/Navbar.java
+++ app/src/main/java/com/example/buildyourownmeal/Navbar.java
@@ -2,18 +2,36 @@ package com.example.buildyourownmeal;
 
 import static com.example.buildyourownmeal.R.*;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.MenuItemCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.NavHostKt;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class Navbar extends AppCompatActivity {
+public class Navbar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +39,95 @@ public class Navbar extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.navbar);
 
+
+        //BOTTOM NAVBAR
+        //HOOK
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
 
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager() .findFragmentById(R.id.nav_host_fragment);
-        assert navHostFragment != null;
-        NavController navCon = navHostFragment.getNavController();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+            NavController navCon = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(bottomNav, navCon);
+        } else {
+            Log.e("Navbar", "NavHostFragment is empty, Check your layout xml");
+        }
 
-        NavigationUI.setupWithNavController(bottomNav, navCon);
 
+        //SIDEBAR
+        //HOOK
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.navbar);
+        NavigationView navigationView = findViewById(R.id.sidebar);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open_sidebar, R.string.close_sidebar);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(false);
+        setToolbarIconSize(toolbar, drawable.baseline_account_circle_24, 110);
+        toolbar.setNavigationOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    //FOR DISPLAYING FRAGMENTS FOR MAIN ACTIVITY/navbar.xml
+    private void hideMainLayout() {
+        findViewById(R.id.toolbar).setVisibility(NavigationView.GONE);
+        findViewById(R.id.sidebar).setVisibility(NavigationView.GONE);
+        findViewById(R.id.bottom_navigation).setVisibility(NavigationView.GONE);
+    }
+
+    private void showMainLayout() {
+        findViewById(R.id.toolbar).setVisibility(NavigationView.VISIBLE);
+        findViewById(R.id.sidebar).setVisibility(NavigationView.VISIBLE);
+        findViewById(R.id.bottom_navigation).setVisibility(NavigationView.VISIBLE);
+    }
+
+    //LOGIC FOR SIDEBAR ITEMS
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.account) {
+            Intent intent = new Intent(this, account.class);
+            startActivity(intent);
+        } else if (id == R.id.orderHis) {
+            Intent intent = new Intent(this, orderHistory.class);
+            startActivity(intent);
+        } else if (id == R.id.contactUs) {
+            Intent intent = new Intent(this, contactUs.class);
+            startActivity(intent);
+        } else if (id == R.id.termsAndCondition) {
+            Intent intent = new Intent(this, termsAndCondition.class);
+            startActivity(intent);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+            showMainLayout();
+        } else {
+            super.getOnBackPressedDispatcher().onBackPressed();
+        }
+
+    }
+
+
+    //ACTION BAR ICON SIZE METHOD
+    private void setToolbarIconSize(Toolbar toolbar, int iconRes, int size) {
+        Drawable drawable = ContextCompat.getDrawable(this, iconRes);
+        if (drawable != null) {
+            Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            toolbar.setNavigationIcon(new BitmapDrawable(getResources(), bitmap));
+        }
     }
 }
--- app/src/main/java/com/example/buildyourownmeal/home_dashboard.java
+++ app/src/main/java/com/example/buildyourownmeal/home_dashboard.java
@@ -1,64 +1,124 @@
 package com.example.buildyourownmeal;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link home_dashboard#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class home_dashboard extends Fragment {
+import com.google.android.material.navigation.NavigationView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class home_dashboard extends Fragment /*implements NavigationView.OnNavigationItemSelectedListener*/ {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DrawerLayout drawerLayout;
 
-    public home_dashboard() {
-        // Required empty public constructor
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_dashboard, container, false);
+
+        //SIDEBAR
+        //HOOK
+        /*Toolbar toolbar = view.findViewById(R.id.toolbar);
+        drawerLayout = view.findViewById(R.id.home);
+        NavigationView navigationView = view.findViewById(R.id.sidebar);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar,R.string.open_sidebar, R.string.close_sidebar);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(false);
+        setToolbarIconSize(toolbar, R.drawable.baseline_account_circle_24, 110);
+        toolbar.setNavigationOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();*/
+
+        return view;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment home_dashboard.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static home_dashboard newInstance(String param1, String param2) {
-        home_dashboard fragment = new home_dashboard();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    //FOR DISPLAYING FRAGMENTS FOR MAIN ACTIVITY/navbar.xml
+    /*private void hideMainLayout() {
+        View view = getView();
+        if (view != null) {
+            view.findViewById(R.id.toolbar).setVisibility(NavigationView.GONE);
+            view.findViewById(R.id.sidebar).setVisibility(NavigationView.GONE);
+            view.findViewById(R.id.bottom_navigation).setVisibility(NavigationView.GONE);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_dashboard, container, false);
+    private void showMainLayout() {
+        View view = getView();
+        if (view != null) {
+            view.findViewById(R.id.toolbar).setVisibility(NavigationView.VISIBLE);
+            view.findViewById(R.id.sidebar).setVisibility(NavigationView.VISIBLE);
+            view.findViewById(R.id.bottom_navigation).setVisibility(NavigationView.VISIBLE);
+        }
     }
+
+    //LOGIC FOR SIDEBAR ITEMS
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.account) {
+            Intent intent = new Intent(getActivity(), account.class);
+            startActivity(intent);
+        } else if (id == R.id.orderHis) {
+            Intent intent = new Intent(getActivity(), orderHistory.class);
+            startActivity(intent);
+        } else if (id == R.id.contactUs) {
+            Intent intent = new Intent(getActivity(), contactUs.class);
+            startActivity(intent);
+        } else if (id == R.id.termsAndCondition) {
+            Intent intent = new Intent(getActivity(), termsAndCondition.class);
+            startActivity(intent);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /*@Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+            showMainLayout();
+        } else {
+            super.getOnBackPressedDispatcher().onBackPressed();
+        }
+
+    }
+
+    //ACTION BAR ICON SIZE METHOD
+    private void setToolbarIconSize(Toolbar toolbar, int iconRes, int size) {
+        Drawable drawable = ContextCompat.getDrawable(getContext(),  iconRes);
+        if (drawable != null) {
+            Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            toolbar.setNavigationIcon(new BitmapDrawable(getResources(), bitmap));
+        }
+    }*/
 }
