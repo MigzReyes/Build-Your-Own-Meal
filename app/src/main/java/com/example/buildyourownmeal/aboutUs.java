e6d2870802f0dfeb59c5aea52fb4580d26effde2
Haku <johnmigzreyes08@example.com>
2025-04-09 01:24:15 +0800

    MIGZ REYES
    CHANGES
    
    - About Us updated

--- app/src/main/java/com/example/buildyourownmeal/aboutUs.java
+++ app/src/main/java/com/example/buildyourownmeal/aboutUs.java
@@ -7,13 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class aboutUs extends Fragment {
+
+    //LOCAL VARIABLE
+    TextView fragName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+
+        //VARIABLE REFERENCE
+        fragName = view.findViewById(R.id.fragName);
+        fragName.setText(R.string.aboutUs);
+
+
+        return view;
     }
 }
