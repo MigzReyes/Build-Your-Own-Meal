725e0ae70ed5299628fbbc4177281942e3884418
Haku <johnmigzreyes08@example.com>
2025-04-11 20:29:45 +0800

    MIGZ REYES
    CHANGES
    
    - update activity_log_in.xml
    - update activity_sign_up.xml
    - added splashscreen.xml
    - added introduction_screen

---
+++ app/src/main/java/com/example/buildyourownmeal/introduction_screen.java
@@ -0,0 +1,62 @@
+package com.example.buildyourownmeal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class introduction_screen extends AppCompatActivity {
+
+    //VARIABLE DECLARATION
+    Button logInBtn, signUpBtn, guestBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_introduction_screen);
+
+        //REFERENCE
+        logInBtn = findViewById(R.id.logInBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        guestBtn = findViewById(R.id.guestBtn);
+
+        //INTENT
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(introduction_screen.this, logIn.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(introduction_screen.this, signUp.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+        guestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(introduction_screen.this, Navbar.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/buildyourownmeal/splashscreen.java
@@ -0,0 +1,26 @@
+package com.example.buildyourownmeal;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splashscreen);
+
+        Intent intent = new Intent(splashscreen.this, Navbar.class);
+        startActivity(intent);
+
+        //THIS WILL REMOVE IT FROM THE BACKSTACK
+        finish();
+    }
+}
