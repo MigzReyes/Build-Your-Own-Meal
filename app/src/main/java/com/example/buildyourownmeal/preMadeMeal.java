813c9144ee668ad325b94b285385f83364bed159
Haku <johnmigzreyes08@example.com>
2025-04-21 18:03:39 +0800

    MIGZ REYES
    CHANGES
    
    - added activity_pre_made_meal.xml

---
+++ app/src/main/java/com/example/buildyourownmeal/preMadeMeal.java
@@ -0,0 +1,21 @@
+package com.example.buildyourownmeal;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class preMadeMeal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pre_made_meal);
+
+
+    }
+}
