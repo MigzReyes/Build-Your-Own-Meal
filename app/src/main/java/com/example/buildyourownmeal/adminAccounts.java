effaacd5f3d05a51be2d04d0787d4195e14a8147
Haku <johnmigzreyes08@example.com>
2025-05-01 16:45:13 +0800

    MIGZ REYES
    CHANGES
    
    - added activity_admin_accounts.xml.xml.xml
    - added activity_admin_add_edit_user.xml.xml.xml

--- app/src/main/java/com/example/buildyourownmeal/adminAccounts.java
+++ app/src/main/java/com/example/buildyourownmeal/adminAccounts.java
@@ -1,16 +1,33 @@
 package com.example.buildyourownmeal;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class adminAccounts extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class adminAccounts extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+
+    private Button addUserBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +35,68 @@ public class adminAccounts extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_accounts);
 
+        //REFERENCE
+        addUserBtn = findViewById(R.id.addUserBtn);
+
         //STATUS BAR
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             WindowInsetsControllerCompat windowInsetsController = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
             windowInsetsController.setAppearanceLightStatusBars(true);
         }
-        
+
+        //SIDE BAR
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.adminAccount);
+        NavigationView navigationView = findViewById(R.id.sidebar);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_sidebar, R.string.close_sidebar);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        toolbar.setTitleTextColor(getColor(R.color.white));
+        toolbar.setNavigationOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //SEARCH BAR
+        SearchView searchbar = findViewById(R.id.searchbar);
+        searchbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchbar.setIconified(false);
+            }
+        });
+
+        //ADD USER BUTTON
+        addUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(adminAccounts.this, adminAddEditUser.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.admin) {
+            Intent intent = new Intent(this, admin.class);
+            startActivity(intent);
+        } else if (id == R.id.adminOrders) {
+            Intent intent = new Intent(this, adminOrders.class);
+            startActivity(intent);
+        } else if (id == R.id.adminMeals) {
+            Intent intent = new Intent(this, adminMeals.class);
+            startActivity(intent);
+        } else if (id == R.id.adminMenu) {
+            Intent intent = new Intent(this, adminMenu.class);
+            startActivity(intent);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
